a2405f1cb44e35bf507fc592ef6288a4802b5e87
jn19392 <jn19392@bristol.ac.uk>
2020-02-25 20:19:44 +0000

    Commenting the files containing initial values

--- BoomBustModel.py
+++ BoomBustModel.py
@@ -1,21 +1,24 @@
 from CurrentModel import StateChanges
-ActiveToDormant = 0.1
-ActiveToInfluencer = 0.0001
-InfluencerToActive = 0.1
-
-DormantToActive = 0.1
-DormantToNonUsers = 0.1
-
-RecruitmentRateFromFriends = 1
-RecruitmentRateFromInfluencers = 10
-
+"""
+Define the initial values and coefficients for the model.
+These should create a boom bust curve.
+"""
+#The initial states of the users.
 POPULATION = 10**4
 ActiveUsers = 0
 DormantUsers = 0
 Influencers = 100
 NonUsers = POPULATION - Influencers
 Time = 150
-
+#The coefficients for the various state changes.
+ActiveToDormant = 0.1
+ActiveToInfluencer = 0.0001
+InfluencerToActive = 0.1
+DormantToActive = 0.1
+DormantToNonUsers = 0.1
+RecruitmentRateFromFriends = 1
+RecruitmentRateFromInfluencers = 10
+#Calling the main function to run the model.
 StateChanges(ActiveToDormant, ActiveToInfluencer, InfluencerToActive, DormantToActive,
              DormantToNonUsers, RecruitmentRateFromFriends, RecruitmentRateFromInfluencers,
              ActiveUsers, DormantUsers, Influencers, NonUsers, POPULATION, Time)
